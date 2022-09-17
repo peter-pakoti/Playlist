0b9d6156711a627e47f28e0f593504658915d40f
peter-pakoti <107296263+peter-pakoti@users.noreply.github.com>
2022-09-17 23:05:47 +1200

    ran migrations

---
+++ server/db/migrations/20220917105958_playlist.js
@@ -0,0 +1,20 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.createTable('playlist', (table) => {
+    table.increments()
+    table.string('songTitle')
+    table.string('artist')
+    table.string('review')
+  })
+}
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.dropTable('playlist')
+}
