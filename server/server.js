9e672ec3ffc6a17e5e5b2bec9ecdca86eabee468
peter-pakoti <107296263+peter-pakoti@users.noreply.github.com>
2022-09-17 23:30:53 +1200

    added playlist routes to server.js

--- server/server.js
+++ server/server.js
@@ -1,9 +1,14 @@
 const express = require('express')
 const path = require('path')
-
+const playlistRoutes = require('./routes/playlist')
 const server = express()
 
 server.use(express.json())
 server.use(express.static(path.join(__dirname, 'public')))
+server.use('/api/playlist', playlistRoutes)
+
+server.get('*', (req, res) => {
+  res.sendFile(path.resolve('server/public/index.html'))
+})
 
 module.exports = server
