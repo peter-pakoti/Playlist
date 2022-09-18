d4b8d4404340f6faa9c160e76a66ad048bda22a9
peter-pakoti <107296263+peter-pakoti@users.noreply.github.com>
2022-09-18 22:41:42 +1200

    created addPlaylist function in db/db.js

--- server/db/db.js
+++ server/db/db.js
@@ -4,4 +4,15 @@ function getPlaylist(db = connection) {
   return db('playlist').select()
 }
 
-module.exports = { getPlaylist }
+function addPlaylist(newPlaylist, db = connection) {
+  return db('playlist').insert(newPlaylist)
+}
+
+// function addFruit(fruit, db = connection) {
+//   return db('fruits').insert(fruit)
+// }
+
+module.exports = {
+  getPlaylist,
+  addPlaylist,
+}
