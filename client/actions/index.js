743f9e87334ef0af8854fda9b41019c7fb6b23e6
peter-pakoti <107296263+peter-pakoti@users.noreply.github.com>
2022-09-19 11:41:33 +1200

    created createPlaylist and makePlaylist functions in actions/index.js

--- client/actions/index.js
+++ client/actions/index.js
@@ -1,6 +1,7 @@
-import { getPlaylist } from '../apis/playlist'
+import { getPlaylist, addPlaylist } from '../apis/playlist'
 
 export const SET_PLAYLIST = 'SET_PLAYLIST'
+export const ADD_PLAYLIST = 'ADD_PLAYLIST'
 
 export function setPlaylist(playlist) {
   return { type: SET_PLAYLIST, payload: playlist }
@@ -10,6 +11,19 @@ export function fetchPlaylist() {
   return (dispatch) => {
     return getPlaylist().then((playlist) => {
       dispatch(setPlaylist(playlist))
+      return null
+    })
+  }
+}
+
+export function createPlaylist(playlist) {
+  return { type: ADD_PLAYLIST, payload: playlist }
+}
+
+export function makePlaylist() {
+  return (dispatch) => {
+    return addPlaylist().then((playlist) => {
+      dispatch(createPlaylist(playlist))
       return null
     })
   }
